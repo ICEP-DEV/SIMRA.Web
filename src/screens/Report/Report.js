1d4bfa13f41d18a438a4c9e5168b7b0a4dd00346
Sibusiso Mvundla <69355799+SbudaRabbit@users.noreply.github.com>
2023-10-02 15:28:18 +0200

    Add files via upload

--- src/screens/Report/Report.js
+++ src/screens/Report/Report.js
@@ -1,96 +1,171 @@
 import React, { useEffect, useState } from 'react';
 import './Report.css';
 import axios from 'axios';
-import Admin_Side_Bar from '../Admin_Side_Bar/Admin_Side_Bar'
+import AdminSideBar from '../../Admin_Side_Bar/Admin_Side_Bar'
 
 function ReportTable() {
 
-  const [Province, setProvince] = useState([])
-  const [ReportData, setReportData] = useState([])
+  const [Provinces, setProvinces] = useState([])
+  const [Report, setReport] = useState([])
+  const [GetMonths, setGetMonths] = useState([])
+  const [GetYears, setGetYears] = useState([])
+  const [CurrentMonth, setCurrentMonth] = useState('')
+  const [CurrentYear, setCurrentYear] = useState('')
+  const [Province, setProvince] = useState('')
+  const [FoundReport, setFoundReport] = useState(false)
 
   useEffect(() => {
+    var date = new Date();
+    function getDates() {
+      var getMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+      var getYears = []
+      var year = date.getFullYear()
+      // get months from 1 - 12
+      // for (let k = 0; k < 12; k++) {
+
+      //   getMonths.push((k + 1).toString().padStart(2, '0'))
+      // }
+      // get year from 2010 to current year
+      for (let k = 2009; k < year; k++) {
+        getYears.push(k + 1)
+      }
+      // set year and months to the states
+      setGetYears(getYears)
+      setGetMonths(getMonths)
+      // set current year and month to the states
+      setCurrentYear(year)
+      setCurrentMonth(getMonths[date.getMonth()])
+      //setCurrentMonth((date.getMonth()+1).toString().padStart(2, '0'))
+
+
+    }
+
     axios.get("http://localhost:3001/api/get_provinces").then(response => {
-      setProvince(response.data.results)
+      setProvinces(response.data.results)
 
     }, err => {
       console.log(err)
     })
-  }, [Province]);
+    getDates()
+  }, []);
+
+  async function display_search_report() {
+    var search_report = {
+      province_id: Province,
+      date: CurrentMonth + '/' + CurrentYear
+    }
+    var report_results = await axios.post('http://localhost:3001/api/get_monthly_reports', search_report)
+    setReport(report_results.data.results)
+    setFoundReport(report_results.data)
+    console.log(report_results.data)
+  }
 
   return (
-    <div className="body">
-      <div className="content">
-        <div className="left">
-          <Admin_Side_Bar />
-        </div>
-        <div className="container">
+    <div className='hero-all'>
+      <div className='sidenav'>
+        <AdminSideBar />
+      </div>
+      <div className='main-all'>
+        <div className='content'>
+          <div className='container-wrap'>
+          <h2>Sanitary risk core (percentage and risk characterization rating)</h2>
+            <div className='date'>
+              <select value={CurrentYear} onChange={(e) => setCurrentYear(e.target.value)}>
+                {GetYears.map((years, xid) => (
+                  <option key={xid}>{years}</option>
+                ))}
+              </select>
+
+              <select value={CurrentMonth} onChange={(e) => setCurrentMonth(e.target.value)}>
+                {GetMonths.map((months, xid) => (
+                  <option key={xid} value={months} >{months}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className='province'>
+              <select onChange={(e) => setProvince(e.target.value)}>
+                {Provinces.map((province, xid) => (
+                  <option key={xid} value={province.province_id} >{province.province_name}</option>
+                ))}
+              </select>
+            </div>
+            <div><button onClick={display_search_report} className="btn btn-primary">Search</button></div>
 
 
-          <div>
-            <div>
-              <div className=''>
-                <label>year</label>
-                <label>month</label>
-              </div>
-              <div>
-                <select >
-                  {Province.map((report, xid) => (
-                    <option key={xid} value={report.province_id}>{report.province_name}</option>
-                  ))}
-                </select>
-              </div>
+            <div className='reports'>
+              {(FoundReport.success === true) && (<div >
+                {Report.map((report, xid) => (
+                 <div className='card-rows'>
+                 <div className='card' key={xid}>
+                    <div className='card-body'>
+                      {/* Display colour based on average score % */}
+                      {(report.total_avarage <= 25) && (
+                      <div className='card-circle' style={{backgroundColor:"rgba(0, 128, 0, 0.719)"}}><label className='sanitary_score'>{report.total_avarage}%</label></div>)}
+                      {(report.total_avarage >= 26 && report.total_avarage <= 50) && (
+                      <div className='card-circle' style={{backgroundColor:"rgba(255, 255, 0, 0.733)"}}><label className='sanitary_score'>{report.total_avarage}%</label></div>)}
+                      {(report.total_avarage >= 51 && report.total_avarage <= 75) && (
+                      <div className='card-circle' style={{backgroundColor:"rgb(201, 199, 105)"}}><label className='sanitary_score'>{report.total_avarage}%</label></div>)}
+                      {(report.total_avarage >= 76) && (
+                      <div className='card-circle' style={{backgroundColor:"rgb(201, 199, 105)"}}><label className='sanitary_score'>{report.total_avarage}%</label></div>)}
+
+                      {/* Municipality name */}
+                      <div>
+                        
+                        <label className='catchment'>Catchment and Source:</label><br/>
+                        {report.muni_name}({report.type})
+                        </div>
+                      <div>
+                        <label className='risk_type'>Risk Type:</label><br/>
+                        {report.risk_type}</div>
+                    </div>
+                  </div>
+                  </div>
+                ))}
+              </div>)}
+              
+              {(FoundReport.success === false) && (<div >
+                <label>{FoundReport.message}</label>
+
+              </div>)}
+
 
             </div>
-            <table className="report">
+            {/* <table className="report">
               <thead className='tableHead'>
                 <tr>
-                  {/*  */}
                   <th className="thh">Municipality(Source)</th>
-                  <th className="thh">Thembisile Hani (Vlaaglagte Dam)</th>
-                  <th className="thh">Dr JS Moroka (Witbank Dam)</th>
-                  <th className="thh">Dr ND Zuma (Tugela Fury River)</th>
-                  <th className="thh">Tshidzini (Ngwedi River)</th>
-                  <th className="thh">Maniini (Livuvhu)</th>
-                  <th className="thh">King Dalinyebo (Kwageza River)</th>
+                  {Report.map((report, xid) => {
+                    <th className="thh" key={xid}>{report.muni_name}</th>
+                  })}
                 </tr>
               </thead>
+
               <tbody className='tableData'>
                 <tr>
                   <td>Sanitary Score</td>
-                  <td>2</td>
-                  <td>3</td>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>4</td>
-                  <td>5</td>
+                  {Report.map((report, xid) => {
+                    // <td key={xid}>{report}</td>
+                  })}
                 </tr>
                 <tr>
                   <td>Score Percentage</td>
-                  <td>42%</td>
-                  <td>70%</td>
-                  <td>10%</td>
-                  <td>42%</td>
-                  <td>75%</td>
-                  <td>90%</td>
+                  {Report.map((report, xid) => {
+                    <td key={xid}>{report.total_avarage}%</td>
+                  })}
                 </tr>
                 <tr>
                   <td>Risk Rating</td>
-                  <td>Medium Risk</td>
-                  <td>High Risk</td>
-                  <td>Low Risk</td>
-                  <td>Medium Risk</td>
-                  <td>High Risk</td>
-                  <td>Very High Risk</td>
+                  {Report.map((report, xid) => {
+                    <td key={xid}>{report.risk_type}</td>
+                  })}
                 </tr>
               </tbody>
-            </table>
-
+            </table> */}
           </div>
         </div>
       </div>
     </div>
-
-
 
   );
 }
