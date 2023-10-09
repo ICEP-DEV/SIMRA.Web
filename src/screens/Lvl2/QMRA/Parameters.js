942176a6e09015c3a3111b6391724ed2b4b99eb5
Paul <paul8tsel@gmail.com>
2023-10-09 06:28:59 +0200

    modified:   src/screens/Lvl2/QMRA/Parameters.js

--- src/screens/Lvl2/QMRA/Parameters.js
+++ src/screens/Lvl2/QMRA/Parameters.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/* import React, { useState } from 'react';
 
 const qmraParameters = {
   'Campylobacter jejun': { model: 'Beta-Poisson', alpha: 0.145, beta: 7.45 },
@@ -23,7 +23,8 @@ function QMRAApp() {
   const [result, setResult] = useState('');
 
   const calculateResult = () => {
-    let calculatedResult = 0;
+    if (selectedOrganism === 'Other' && model && count && alpha && beta) {
+      let calculatedResult = 0;
 
       if (model === 'Beta-Poisson') {
         calculatedResult = calculateBetaPoisson(alpha, beta, count);
@@ -32,11 +33,8 @@ function QMRAApp() {
         } else if (selectedOrganism === 'Giardia lambia') {
           calculatedResult = calculateExponentialForGiardia(k, count);
         }
-      
-        if (selectedOrganism === 'Other' && model && count && alpha && beta) {
-          
  
-      setResult(`Model Type: ${model}, Calculated Result: ${calculatedResult}`);
+      setResult(`Calculated Result: ${calculatedResult}`);
     } else {
       setResult('');
     }
@@ -63,6 +61,10 @@ function QMRAApp() {
     return calculatedResult;
   };
 
+  const calculateOther = () => {
+    // Perform other calculations here
+  }
+
   return (
     <div style={styles.container}>
       <h1 style={styles.header}>QMRA Parameters</h1>
@@ -83,6 +85,7 @@ function QMRAApp() {
             {organism}
           </option>
         ))}
+
         <option value="Other">Other</option>
       </select>
       {selectedOrganism === 'Other' && (
@@ -119,7 +122,7 @@ function QMRAApp() {
           />
           {model === 'exponential' && (
             <>
-              {selectedOrganism !== 'Cryptosporidium parvum' && selectedOrganism !== 'Giardia lambia' && (
+              {selectedOrganism !== 'Cryptosporidium parvum' && (
                 <input
                   style={styles.input}
                   placeholder="Enter r"
@@ -128,7 +131,7 @@ function QMRAApp() {
                   onChange={(e) => setr(e.target.value)}
                 />
               )}
-              {selectedOrganism !== 'Cryptosporidium parvum' && selectedOrganism !== 'Giardia lambia' && (
+              {selectedOrganism !== 'Giardia lambia' && (
                 <input
                   style={styles.input}
                   placeholder="Enter k"
@@ -177,3 +180,197 @@ const styles = {
 };
 
 export default QMRAApp;
+ */
+import React, { useState } from 'react';
+
+const qmraParameters = {
+  'Campylobacter jejun': { model: 'Beta-Poisson', alpha: 0.145, beta: 7.45 },
+  'E-Coli O157:H7': { model: 'Beta-Poisson', alpha: 0.4, beta: 45.9 },
+  'Salmonella Typhi': { model: 'Beta-Poisson', alpha: 0.21, beta: 49.78 },
+  'S. flexneri': { model: 'Beta-Poisson', alpha: 0.265, beta: 1480 },
+  'Vibrio cholerae': { model: 'Beta-Poisson', alpha: 0.169, beta: 2305 },
+  'Cryptosporidium parvum': { model: 'exponential', r: 0.059 },
+  'Entamoeba coli': { model: 'Beta-Poisson', alpha: 0.79433, beta: 5.40789 },
+  'Giardia lambia': { model: 'exponential', k: 0.01199 },
+  'Other': { model: '', alpha: 0, beta: 0 },
+};
+
+export default function QMRAApp() {
+  const [selectedOrganism, setSelectedOrganism] = useState('Campylobacter jejun');
+  const [count, setCount] = useState('');
+  const [alpha, setAlpha] = useState('');
+  const [beta, setBeta] = useState('');
+  const [r, setR] = useState('');
+  const [k, setK] = useState('');
+  const [model, setModel] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculateResult = () => {
+    if (selectedOrganism === 'Other' && model && count && alpha && beta) {
+      let calculatedResult = 0;
+
+      if (model === 'Beta-Poisson') {
+        calculatedResult = calculateBetaPoisson(alpha, beta, count);
+      } if (selectedOrganism === 'Cryptosporidium parvum' && model === 'exponential') {
+        calculatedResult = calculateExponentialForCryptosporidium(r, count);
+      } else if (selectedOrganism === 'Giardia lambia' && model === 'exponential') {
+        calculatedResult = calculateExponentialForGiardia(k, count);
+      }
+
+      setResult(`Model Type: ${model}, Calculated Result: ${calculatedResult}`);
+    } else {
+      setResult('');
+    }
+  };
+
+  const calculateExponentialForCryptosporidium = (r, count) => {
+    // Implement the exponential calculation for Cryptosporidium parvum
+    // Your formula here 
+    const calculatedResult = 1 - Math.pow(2.71828, -r * count);
+    return calculatedResult;
+  };
+
+  const calculateExponentialForGiardia = (k, count) => {
+    // Implement the exponential calculation for Giardia lambia
+    // Your formula here 
+    const calculatedResult = 1 - Math.pow(2.71828, -k * count);
+    return calculatedResult;
+  };
+
+  const calculateBetaPoisson = (alpha, beta, count) => {
+    // Perform the Beta-Poisson calculation here  
+    const calculatedResult = 1 - (1 + (count / beta)) - alpha;
+    return calculatedResult;
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.header}>QMRA Parameters</h1>
+      <select
+        value={selectedOrganism}
+        onChange={(e) => {
+          setSelectedOrganism(e.target.value);
+          setResult('');
+        }}
+      >
+        {Object.keys(qmraParameters).map((organism) => (
+          <option key={organism} value={organism}>
+            {organism}
+          </option>
+        ))}
+        <option value="Other">Other</option>
+      </select>
+      {selectedOrganism !== 'Other' && model === 'Beta-Poisson' && (
+        <div>
+          <label>Selected Model: {model}</label>
+          <input
+            style={styles.input}
+            placeholder="Enter Count"
+            type="number"
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <input
+            style={styles.input}
+            placeholder="Enter Alpha"
+            type="number"
+            value={alpha}
+            onChange={(e) => setAlpha(e.target.value)}
+          />
+          <input
+            style={styles.input}
+            placeholder="Enter Beta"
+            type="number"
+            value={beta}
+            onChange={(e) => setBeta(e.target.value)}
+          />
+        </div>
+      )}
+      {(selectedOrganism === 'Cryptosporidium parvum' || selectedOrganism === 'Giardia lambia') && model === 'exponential' && (
+        <div>
+          <input
+            style={styles.input}
+            placeholder="Enter r"
+            type="number"
+            value={r}
+            onChange={(e) => setR(e.target.value)}
+          />
+          <input
+            style={styles.input}
+            placeholder="Enter Count"
+            type="number"
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          {selectedOrganism === 'Giardia lambia' && (
+            <input
+              style={styles.input}
+              placeholder="Enter k"
+              type="number"
+              value={k}
+              onChange={(e) => setK(e.target.value)}
+            />
+          )}
+        </div>
+      )}
+      {selectedOrganism === 'Other' && (
+        <div>
+          <input
+            style={styles.input}
+            placeholder="Enter Organism"
+            onChange={(e) => setSelectedOrganism(e.target.value)}
+          />
+          <input
+            style={styles.input}
+            placeholder="Enter Model"
+            onChange={(e) => setModel(e.target.value)}
+          />
+          <input
+            style={styles.input}
+            placeholder="Enter Count"
+            type="number"
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <input
+            style={styles.input}
+            placeholder="Enter Alpha"
+            type="number"
+            step="0.01"
+            onChange={(e) => setAlpha(e.target.value)}
+          />
+          <input
+            style={styles.input}
+            placeholder="Enter Beta"
+            type="number"
+            step="0.01"
+            onChange={(e) => setBeta(e.target.value)}
+          />
+        </div>
+      )}
+      <button onClick={calculateResult}>Calculate</button>
+      {result !== '' && <p style={styles.result}>{result}</p>}
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '20px',
+  },
+  header: {
+    fontSize: '20px',
+    marginBottom: '20px',
+  },
+  input: {
+    width: '200px',
+    fontSize: '16px',
+    marginBottom: '10px',
+    padding: '10px',
+    border: '1px solid blue',
+  },
+  result: {
+    fontSize: '18px',
+    marginTop: '20px',
+  },
+};
