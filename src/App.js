3dd6801b66aeddd6aaa1251baee644493c4310e9
Sibusiso Mvundla <69355799+SbudaRabbit@users.noreply.github.com>
2023-11-03 11:36:48 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './screens/Login/Login';
+import Home from './screens/Home/Home'
 import SamplingData from './screens/SamplingData/SamplingData';
 import Level1 from './screens/Level1/Level1';
-import H2S from './screens/H2S/H2S';
 import SanitaryInpection from './screens/SanitaryInpection/SanitaryInpection';
-import DataResults from './screens/AnalysisResults/AnalysisResults';
+import H2S from './screens/H2S/H2S';
+import Level2 from './screens/Level2/Level2';
+import FibAnalysis from './screens/FIBAnlysis/AnalyseFIB';
+import QMRA from './screens/QMRA/Parameters'
+import Municipality from './screens/Municipality/Municipality';
+import Report from './screens/Municipality_Report/Report'
+import Survay_Report from './screens/Survay_Report/Survay_Report';
+import H2S_Report from './screens/H2S_Report/H2S_Report';
+import User_H2S_Logs from './screens/User_H2S_Logs/User_H2S_Logs';
+import User_Sanitary_Survay_Logs_Reports from './screens/User_Sanitary_Survay_Logs_Reports/User_Sanitary_Survay_Logs_Reports';
+import User_QMRA_logs from './screens/User_QMRA_logs/User_QMRA_logs';
+import MST from './screens/MST/MST';
+import H2S_Logs_Reports from './screens/H2S_Logs_Reports/H2S_Logs_Reports';
+/*import DataResults from './screens/AnalysisResults/AnalysisResults';
+*/
+ import  UserProfileUpdate from './screens/Profile/Profile';
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={<Login />} />
+        <Route exact path='/home' element={<Home />} />
         <Route exact path='/sampling_data' element={<SamplingData />} />
-        <Route exact path='/h2s_testing' element={<H2S />} />
-        <Route exact path='/level1' element={<Level1 />} />
+        <Route exact path='/h2s_survey' element={<Level1 />} />
         <Route exact path='/sanitaryInpection' element={<SanitaryInpection />} />
+        <Route exact path='/h2s_testing' element={<H2S />} />
+        <Route exact path='/level2' element={<Level2 />} />
+        <Route exact path='/fib_analysis' element={<FibAnalysis />} />
+        <Route exact path='/qmra' element={<QMRA />} />
+        {/* <Route exact path='/municipality' element={<Municipality />} /> */}
+        <Route exact path='/municipality' element={<Report />} />
+        <Route exact path='/sanitary_report' element={<Survay_Report />} />
+        <Route exact path='/h2s_report' element={<H2S_Report />} />
+        <Route exact path='/profile' element={< UserProfileUpdate />} />
+
+        
+        
+        {/*
         <Route exact path='/data_results' element={<DataResults />} />
+      
+         */}
+
       </Routes>
-    </Router> 
+    </Router>
   );
 }
 export default App;
