5ba97749a29133dae13d542e595e14d421d157f8
Dipono <64579128+Dipono@users.noreply.github.com>
2023-11-28 13:28:27 +0200

    Update API.js

--- src/API/API.js
+++ src/API/API.js
@@ -1,7 +1,2 @@
-import axios from "axios";
-
-//const api = 'http://localhost:3001/api/'
-const api = 'http://3.252.142.23:3001/'
-const API = {
-    
-}
+//export const api = 'http://localhost:3001/api/'
+export const api = 'http://3.252.142.23:3001/'
