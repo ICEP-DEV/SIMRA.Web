9c595a84cda74d392b828a7b97c19a0d50230d11
unknown <joel.buti97@gmail.com>
2023-12-04 16:01:59 +0200

    update

--- src/screens/Municipality/Municipality.js
+++ src/screens/Municipality/Municipality.js
@@ -37,6 +37,12 @@ function Municipality() {
         var newId = {
             province_id: province_id
         }
+
+        var tempFilter = AllMunicipalities
+
+        setFiteredMunicipalities(tempFilter.filter(muni => {
+            return muni.province_id?.toLocaleLowerCase().includes(province_id?.toLocaleLowerCase())
+        }))
         setExpandSelectedProvince(newId)
     }
     return (
@@ -46,90 +52,92 @@ function Municipality() {
                 <Header />
                 <div className='container-wrapper mb-5'>
                     <h2>Simra Reports</h2>
-                    <div className="section-reports">
+                    <div className='reports' >
                         <div id='today_report' className='section-report'>
                             <h5>Todays Report</h5>
                             <h6>0 &nbsp;Reported</h6>
                         </div>
-
-                        <div className='section-report'>
-                            <h5>Total Reports For Every province</h5>
-                            {IsProvinces && <>
-                            {Provinces.map((prov, xid)=>(
-                                <button className='collapsible' onClick={() => collapse(prov.province_id)}>{prov.province_name}</button>
-                            ))}
-                                {IsMunicipalities && <>
-                                    {ExpandSelectedProvince.province_id === 'gau' &&
-                                        <div className='drop-province' id='collapse1'>
-                                            <div className='province-content'>
-                                                <table>
-                                                    <th>Municipality</th>
-                                                    <th>results</th>
-                                                    <tr>
-                                                        <td>Thswane </td>
-                                                        <td>10 </td>
-                                                    </tr>
-                                                    <tr> 
-                                                        <td>Metropo </td>
-                                                        <td>30 </td>
-                                                    </tr> <tr>
-                                                        <td>Jozi </td>
-                                                        <td>500 </td>
-                                                    </tr>
-                                                </table>
-                                            </div>
+                        <div className="section-reports">
+                            <div className='section-report' id='report_per_prov'>
+                                <h5>Total Reports For Every province</h5>
+                                {IsProvinces && <>
+                                    {Provinces.map((prov, xid) => (
+                                        <div key={xid}>
+                                            <table>
+                                                <tr onClick={() => collapse(prov.province_id)} className='collapsible' >
+                                                    <td className='province_name'>{prov.province_name}</td>
+                                                    <td className='province_count'>{prov.province_total}</td>
+                                                </tr>
+                                            </table>
+                                            {ExpandSelectedProvince.province_id === prov.province_id && <>
+                                                <div className='drop-province' id='collapse1'>
+                                                    <div className='province-content'>
+                                                        <table>
+                                                            <th className='municipal_name'>Municipality</th>
+                                                            <th className='municipal_count'>results</th>
+                                                            {FiteredMunicipalities.map((muni, xid) => (
+                                                                <tr key={xid}>
+                                                                    <td className='municipal_name'>{muni.muni_name} </td>
+                                                                    <td className='municipal_count'>{muni.muni_count}</td>
+                                                                </tr>
+                                                            ))}
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </>}
                                         </div>
-                                    }
+                                    ))} 
                                 </>}
 
-                            </>}
 
-
-                            <button className='collapsible' onClick={() => collapse('lim')}>Limpopo</button>
-                            {ExpandSelectedProvince.province_id === 'lim' &&
-                                <div className='drop-province' id='collapse2'>
-                                    <div className='province-content'>
-                                        <table>
-                                            <th>Municipality</th>
-                                            <th>results</th>
-                                            <tr>
-                                                <td>Vembe </td>
-                                                <td>100 </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Musina </td>
-                                                <td>300 </td>
-                                            </tr> <tr>
-                                                <td>Lethabo </td>
-                                                <td>50 </td>
-                                            </tr>
-                                        </table>
+                                {/* <button className='collapsible' onClick={() => collapse('lim')}>Limpopo</button>
+                                {ExpandSelectedProvince.province_id === 'lim' &&
+                                    <div className='drop-province' id ='collapse2'>
+                                        <div className='province-content'>
+                                            <table>
+                                                <th>Municipality</th>
+                                                <th>results</th>
+                                                <tr>
+                                                    <td>Vembe </td>
+                                                    <td>100 </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Musina </td>
+                                                    <td>300 </td>
+                                                </tr> <tr>
+                                                    <td>Lethabo </td>
+                                                    <td>50 </td>
+                                                </tr>
+                                            </table>
+                                        </div>
                                     </div>
-                                </div>
-                            }
+                                } */}
 
 
+                            </div>
+
+                            <div className='section-report'>
+                                <h5>Total Reports</h5>
+                                <h6>0 &nbsp;Reportednjhvjhuhdshbgudibugeuwg89gew</h6>
+                            </div>
+
+                            <div className='section-report'>
+                                <h5>Todays Report</h5>
+                                <h6>0 &nbsp;Reported</h6>
+                            </div>
+
+                            <div className='section-report'>
+                                <h5>Todays Report</h5>
+                                <h6>0 &nbsp;Reported</h6>
+                            </div>
+
+                            <div className='section-report'>
+                                <h5>Todays Report</h5>
+                                <h6>0 &nbsp;Reported</h6>
+                            </div>
                         </div>
 
-                        <div className='section-report'>
-                            <h5>Total Reports</h5>
-                            <h6>0 &nbsp;Reportednjhvjhuhdshbgudibugeuwg89gew</h6>
-                        </div>
 
-                        <div className='section-report'>
-                            <h5>Todays Report</h5>
-                            <h6>0 &nbsp;Reported</h6>
-                        </div>
-
-                        <div className='section-report'>
-                            <h5>Todays Report</h5>
-                            <h6>0 &nbsp;Reported</h6>
-                        </div>
-
-                        <div className='section-report'>
-                            <h5>Todays Report</h5>
-                            <h6>0 &nbsp;Reported</h6>
-                        </div>
                     </div>
                 </div>
             </div>
