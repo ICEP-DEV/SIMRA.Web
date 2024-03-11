98f42939df2066565cf0bb563aabf6e7ba118f56
Paul <paul8tsel@gmail.com>
2024-03-11 11:39:46 +0200

    new file:   src/API/API.js 	new file:   src/Data/methods.js 	new file:   src/Redux/fib.js 	new file:   src/assets/100ml.png 	new file:   src/assets/Boiling_Water.jpg 	new file:   src/assets/Clay_Vessel_Filtration.jpg 	new file:   src/assets/Electric-heating-water-distiller.jpg 	new file:   src/assets/Iodine_Addition.png 	new file:   src/assets/Reverse_Osmosis.jpg 	new file:   src/assets/Solar_Purification.jpg 	new file:   src/assets/Water_Purifier.jpg 	new file:   src/assets/blackstrip.png 	new file:   src/assets/borehole.avif 	new file:   src/assets/borehole.jpg 	new file:   src/assets/chloritab-bucket-jpg.webp 	new file:   src/assets/sample.png 	new file:   src/assets/tapwater.jpg 	new file:   src/assets/tests.jpg 	new file:   src/assets/tube.jpg 	new file:   src/assets/tubes.avif 	new file:   src/assets/tubes.jpg 	new file:   src/assets/tubes3.jpg 	new file:   src/assets/whitestrip.png 	new file:   src/screens/Lvl2/Level2/back.avif 	new file:   src/screens/Lvl2/Level2/back1.jpg 	new file:   src/screens/Lvl2/Level2/bottle.jpg 	new file:   src/screens/Lvl2/Level2/dirtywater.jpeg 	new file:   src/screens/Lvl2/Level2/dwater.avif 	new file:   src/screens/Lvl2/Level2/hand.avif 	new file:   src/screens/Lvl2/Level2/pexels-lisa-fotios-3264779.jpg 	new file:   src/screens/Lvl2/Level2/pexels-photo-4424148.jpeg 	new file:   src/screens/Navbar/Navbar.css 	new file:   src/screens/Navbar/Navbar.js 	new file:   src/screens/Navbar/logo1.png 	new file:   src/screens/Navbar/logo2.png 	new file:   src/screens/animations/component.js 	new file:   src/screens/animations/typer.js

---
+++ src/screens/Navbar/Navbar.js
@@ -0,0 +1,62 @@
+import React,{useEffect} from "react";
+import './Navbar.css';
+import {Link } from "react-router-dom";
+import logo from './logo2.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { remove_details } from "../../Redux/user";
+
+function Navbar(){
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    let user_info = useSelector((state) => state.user.value)
+    useEffect(() => {
+        if (user_info === undefined) {
+            navigate("/")
+        }
+    }, [])
+
+    function Profile() {
+        navigate("/profile")
+    }
+
+    function report() {
+        navigate("/logs")
+    }
+    function data() {
+        navigate("/sampling_data")
+    }
+
+    function risk() {
+        navigate("/")
+    }
+    function qmra() {
+        navigate("/")
+    }
+
+  
+
+    function logout() {
+        dispatch(remove_details());
+        navigate("/")
+    }
+
+    return(
+<div className="topnav">
+
+<div className="navbar-subs"><span className="nav-label">Home</span></div> 
+
+<div className="navbar-subs" onClick={Profile}><span className='nav-label'>Profile</span></div>
+  <div className="navbar-subs" onClick={data}><span className="nav-label">Sampling Data</span></div>
+  <div className="navbar-subs "><div className="wrapper"></div>
+  <Link to={"/home"}> <img className="rounded-img" src={logo}/></Link></div>
+  <div className="navbar-subs" ><span className="nav-label">Report</span></div>
+ <div className="navbar-subs dropdown"><span className="nav-label">Levels</span></div>
+  <div className="navbar-subs split" onClick={logout}><span className="nav-label">Signout</span></div>
+ 
+
+</div>
+    )
+
+}
+export default Navbar;
